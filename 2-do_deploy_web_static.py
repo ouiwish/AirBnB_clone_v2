557660543ea5f5437276f605c892a329bbe404b7
widad <widad.moumkine@gmail.com>
2024-04-04 23:03:29 +0400

    delete

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,9 +1,57 @@
 #!/usr/bin/python3
 # Fabfile to distribute an archive to a web server.
 import os.path
-from fabric.api import env
-from fabric.api import put
-from fabric.api import run
+from fabric.api import env, put, run, execute
+
+env.hosts = ["104.196.168.90", "35.196.46.172"]
+def do_deploy(archive_path):
+    """Distributes an archive to a web server.
+
+    Args:
+        archive_path (str): The path of the archive to distribute.
+    Returns:
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
+    """
+    if not os.path.isfile(archive_path):
+        return False
+
+    file_name = os.path.basename(archive_path)
+    name = os.path.splitext(file_name)[0]
+
+    if put(archive_path, "/tmp/{}".format(file_name)).failed:
+        return False
+
+    commands = [
+        "rm -rf /data/web_static/releases/{}/".format(name),
+        "mkdir -p /data/web_static/releases/{}/".format(name),
+        "tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file_name, name),
+        "rm /tmp/{}".format(file_name),
+        "mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(name, name),
+        "rm -rf /data/web_static/releases/{}/web_static".format(name),
+        "rm -rf /data/web_static/current",
+        "ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name)
+    ]
+
+    # Execute commands in parallel on all hosts
+    results = execute(run_commands, commands, hosts=env.hosts, parallel=True)
+
+    # Check if any command failed
+    if any(result.failed for result in results.values()):
+        return False
+
+    return True
+
+
+def run_commands(commands):
+    """Run multiple commands on a host."""
+    for command in commands:
+        if run(command).failed:
+            return False
+    return True#!/usr/bin/python3
+# Fabfile to distribute an archive to a web server.
+import os.path
+from fabric.api import env, put, run, execute
 
 env.hosts = ["104.196.168.90", "35.196.46.172"]
 
@@ -17,33 +65,39 @@ def do_deploy(archive_path):
         If the file doesn't exist at archive_path or an error occurs - False.
         Otherwise - True.
     """
-    if os.path.isfile(archive_path) is False:
+    if not os.path.isfile(archive_path):
         return False
-    file = archive_path.split("/")[-1]
-    name = file.split(".")[0]
 
-    if put(archive_path, "/tmp/{}".format(file)).failed is True:
+    file_name = os.path.basename(archive_path)
+    name = os.path.splitext(file_name)[0]
+
+    if put(archive_path, "/tmp/{}".format(file_name)).failed:
         return False
-    if run("rm -rf /data/web_static/releases/{}/".
-           format(name)).failed is True:
+
+    commands = [
+        "rm -rf /data/web_static/releases/{}/".format(name),
+        "mkdir -p /data/web_static/releases/{}/".format(name),
+        "tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file_name, name),
+        "rm /tmp/{}".format(file_name),
+        "mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(name, name),
+        "rm -rf /data/web_static/releases/{}/web_static".format(name),
+        "rm -rf /data/web_static/current",
+        "ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name)
+    ]
+
+    # Execute commands in parallel on all hosts
+    results = execute(run_commands, commands, hosts=env.hosts, parallel=True)
+
+    # Check if any command failed
+    if any(result.failed for result in results.values()):
         return False
-    if run("mkdir -p /data/web_static/releases/{}/".
-           format(name)).failed is True:
-        return False
-    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
-           format(file, name)).failed is True:
-        return False
-    if run("rm /tmp/{}".format(file)).failed is True:
-        return False
-    if run("mv /data/web_static/releases/{}/web_static/* "
-           "/data/web_static/releases/{}/".format(name, name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/releases/{}/web_static".
-           format(name)).failed is True:
-        return False
-    if run("rm -rf /data/web_static/current").failed is True:
-        return False
-    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
-           format(name)).failed is True:
-        return False
+
+    return True
+
+
+def run_commands(commands):
+    """Run multiple commands on a host."""
+    for command in commands:
+        if run(command).failed:
+            return False
     return True
